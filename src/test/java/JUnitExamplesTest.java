fdb7fb3b5b855af2e0643c53950ecd141c27c1ca
Andrew <mr.kimpa4i@gmail.com>
2025-03-31 12:10:34 +0300

    add  junit example

---
+++ src/test/java/JUnitExamplesTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitExamplesTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+
+
+    @Test
+    void firstTest() {
+        System.out.println("First Test");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Second Test");
+    }
+
+}
+
+
